0bd1b9b05a541d08c6b917c5119bba2d95bbb820
Laure Chen <73329947+LaureChen@users.noreply.github.com>
2020-12-27 17:13:51 +0800

    R code

---
+++ Assigment 1_R code.R
@@ -0,0 +1,67 @@
+## Download the file
+if(!file.exists("./Desktop")){dir.create("./Desktop")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl,destfile="./Desktop/Dataset.zip",method="curl")
+## Unzip the file
+unzip(zipfile="./Desktop/Dataset.zip", exdir = "./Desktop")
+## Get the list of unzipped files
+path_rf <- file.path("./Desktop")
+files <- list.files(path_rf, recursive=TRUE)
+files
+## Read file
+data_full <- read.csv("./Desktop/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+## PLOT 1
+data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+hist(data1$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
+
+##PLOT 2
+data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(data1$Date), data1$Time)
+data1$Datetime <- as.POSIXct(datetime)
+with(data1, {
+  plot(Global_active_power~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+})
+## Save to file
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
+
+##PLOT 3
+with(data1, {
+  plot(Sub_metering_1~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+## Save to file
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
+
+##PLOT 4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(data1, {
+  plot(Global_active_power~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~Datetime, type="l", 
+       ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~Datetime, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="")
+})
+## Save to file
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off
+ 
